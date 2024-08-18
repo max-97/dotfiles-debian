2a98ec0137cd79f07086429d152f5bb46a800f0c
Max Braun <maxbraun97@web.de>
2024-08-18 16:58:32 +0200

    Add tmux config

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt update && sudo apt upgrade -y
 echo "***"
 echo "Basic programs and libs installation"
 echo "***"
-sudo apt install -y linux-image-amd64 firmware-linux-nonfree curl vim git inxi build-essential zsh tmux ripgrep python3 python3-venv stow bat thefuck
+sudo apt install -y linux-image-amd64 firmware-linux-nonfree curl vim git inxi build-essential zsh tmux ripgrep python3 python3-venv stow bat thefuck xsel
 
 git clone --depth 1 https://github.com/junegunn/fzf.git $dotfiles_dir/.local/share/.fzf
 $dotfiles_dir/.local/share/.fzf/install
@@ -49,6 +49,9 @@ tar xf lazygit.tar.gz lazygit
 install lazygit /usr/local/bin
 rm -rf lazygit.tar.gz
 rm -rf lazygit
+
+# Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
 cd $dotfiles_dir
 stow .
