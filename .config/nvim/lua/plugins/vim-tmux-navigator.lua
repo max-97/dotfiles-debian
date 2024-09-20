2c8d9acf59f7bda2cbc1c12e2843a88cc238add6
Max Braun <13639830+max-97@users.noreply.github.com>
2024-09-20 21:13:36 +0200

    Add vim-tmux-navigator

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,17 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<A-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<A-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<A-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<A-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<A-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	},
+}
