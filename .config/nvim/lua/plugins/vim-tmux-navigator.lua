dfa7fb39dae76a4d7db77a200cf11a9b7690c9b4
Max Braun <13639830+max-97@users.noreply.github.com>
2024-09-20 22:20:29 +0200

    Change vim-tmux-navigator keybindings

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -8,10 +8,10 @@ return {
 		"TmuxNavigatePrevious",
 	},
 	keys = {
-		{ "<A-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<A-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<A-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<A-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<A-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		{ "<A-h>", "<cmd>TmuxNavigateLeft<cr>" },
+		{ "<A-j>", "<cmd>TmuxNavigateDown<cr>" },
+		{ "<A-k>", "<cmd>TmuxNavigateUp<cr>" },
+		{ "<A-l>", "<cmd>TmuxNavigateRight<cr>" },
+		{ "<A-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
 	},
 }
