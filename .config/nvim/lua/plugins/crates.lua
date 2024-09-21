f8276b113ce4e73701eb49e4b5dfcddfe37bc686
Max Braun <13639830+max-97@users.noreply.github.com>
2024-09-21 14:34:28 +0200

    Add crates plugin

---
+++ .config/nvim/lua/plugins/crates.lua
@@ -0,0 +1,12 @@
+return {
+	"saecki/crates.nvim",
+	tag = "stable",
+	event = { "BufRead Cargo.toml" },
+	config = function()
+	    local crates = require("creates")
+        crates.setup()
+
+        local map = vim.keymap.set
+        map("n", "<leader>rcu", function() crates.upgrade_all_crates() end, { desc="Update crates" })
+	end,
+}
