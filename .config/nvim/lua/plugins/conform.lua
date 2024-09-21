eacbf58383ed8f03011ffc37dd23c75162ae259a
Max Braun <13639830+max-97@users.noreply.github.com>
2024-09-21 15:22:41 +0200

    Add format on save to conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,14 +4,12 @@ return {
     opts = {
         formatters_by_ft = {
             lua = { "stylua" },
-            -- css = { "prettier" },
-            -- html = { "prettier" },
         },
 
-        -- format_on_save = {
-        --   -- These options will be passed to conform.format()
-        --   timeout_ms = 500,
-        --   lsp_fallback = true,
-        -- },
+        format_on_save = {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
     },
 }
