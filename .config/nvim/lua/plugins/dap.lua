6e932846aff5e377321817c8c51cf15f3d3d5e4f
Max Braun <13639830+max-97@users.noreply.github.com>
2024-09-21 15:21:50 +0200

    Fix nvim-dap keys setup

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,14 +1,13 @@
 return {
 	"mfussenegger/nvim-dap",
 	keys = {
-		{ "n", "<leader>db", "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle Breakpoint" } },
+		{ "<leader>db", "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle Breakpoint" } },
 		{
-			"n",
 			"<leader>dus",
 			function()
 				local widgets = require("dap.ui.widgets")
-                local sidebar = widgets.sidebar(widgets.scopes)
-                sidebar.open()
+				local sidebar = widgets.sidebar(widgets.scopes)
+				sidebar.open()
 			end,
 			{ desc = "Open debugging sidebar" },
 		},
