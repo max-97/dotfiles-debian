606fbcef3b406e1c002ccd1192c1dd87abf87eed
Max Braun <13639830+max-97@users.noreply.github.com>
2024-09-30 20:07:58 +0200

    Configure awesome for lua lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -8,13 +8,51 @@ return {
 		},
 	},
 	config = function()
-		-- load defaults i.e lua_lsp
-		require("nvchad.configs.lspconfig").defaults()
-
 		local lspconfig = require("lspconfig")
+		local nvlsp = require("nvchad.configs.lspconfig")
+
+		-- default lua from nvchad + awesome wm setup
+		dofile(vim.g.base46_cache .. "lsp")
+		require("nvchad.lsp").diagnostic_config()
+
+		lspconfig.lua_ls.setup({
+			on_attach = nvlsp.on_attach,
+			capabilities = nvlsp.capabilities,
+			on_init = nvlsp.on_init,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = {
+							"vim",
+							"awesome",
+							"awful",
+							"client",
+							"gears",
+							"screeen",
+							"tag",
+							"root",
+						},
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.fn.expand("$VIMRUNTIME/lua"),
+							vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+							vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+							vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+							"${3rd}/luv/library",
+							"${3rd}/busted/library",
+							"/usr/share/awesome/lib",
+							"/usr/share/lua",
+						},
+						maxPreload = 100000,
+						preloadFileSize = 10000,
+					},
+				},
+			},
+		})
 
 		local servers = { "ruff" }
-		local nvlsp = require("nvchad.configs.lspconfig")
 
 		-- lsps with default config
 		for _, lsp in ipairs(servers) do
@@ -24,12 +62,5 @@ return {
 				capabilities = nvlsp.capabilities,
 			})
 		end
-
-		-- configuring single server, example: typescript
-		-- lspconfig.ts_ls.setup {
-		--   on_attach = nvlsp.on_attach,
-		--   on_init = nvlsp.on_init,
-		--   capabilities = nvlsp.capabilities,
-		-- }
 	end,
 }
