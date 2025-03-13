2dbb9b066bd1c09ed9240a24f0225a339ba6fbf1
Max Braun <13639830+max-97@users.noreply.github.com>
2025-03-13 19:55:50 +0100

    Add file to configure user apps

---
+++ .config/awesome/apps.lua
@@ -0,0 +1,14 @@
+local menubar = require("menubar")
+
+terminal = "alacritty"
+-- TODO maybe use other explorer
+explorer = "nautilus"
+browser = "brave-browser"
+-- TODO install and setup rofi
+-- launcher = "rofi -show drun"
+editor = "nvim"
+editor_cmd = terminal .. " -e " .. editor
+
+modkey = "Mod4"
+
+menubar.utils.terminal = terminal
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -6,7 +6,6 @@ pcall(require, "luarocks.loader")
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
-require("awful.autofocus")
 -- Widget and layout library
 local wibox = require("wibox")
 -- Theme handling library
@@ -15,7 +14,6 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 -- Declarative object management
 local ruled = require("ruled")
-local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
@@ -39,19 +37,7 @@ local chosen_theme = "default"
 local theme_path = string.format("%s/themes/%s/theme.lua", gears.filesystem.get_configuration_dir(), chosen_theme)
 beautiful.init(theme_path)
 
--- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
-editor = "nvim"
-editor_cmd = terminal .. " -e " .. editor
-browser = "brave-browser"
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
--- }}}
+require("apps")
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
@@ -81,10 +67,6 @@ mymainmenu = awful.menu({
 })
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
 
 -- {{{ Tag layout
 -- Table of layouts to cover with awful.layout.inc, order matters.
