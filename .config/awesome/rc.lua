768efb5940e3e2b54b9a7afda01c1e9ab954ce80
Max Braun <13639830+max-97@users.noreply.github.com>
2025-03-18 20:35:39 +0100

    Move autofocus to configuration

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -267,9 +267,4 @@ end)
 
 -- }}}
 
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-	c:activate({ context = "mouse_enter", raise = false })
-end)
-
 awful.spawn.with_shell("setxkbmap -layout us -variant intl")
