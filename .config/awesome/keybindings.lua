fbcd0421af004521fc8ccdc2f40eab9fdb77a576
Max Braun <13639830+max-97@users.noreply.github.com>
2025-03-18 20:21:29 +0100

    Remove mouse bindings from keybindings.lua

--- .config/awesome/keybindings.lua
+++ .config/awesome/keybindings.lua
@@ -3,16 +3,6 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
--- {{{ Mouse bindings
-awful.mouse.append_global_mousebindings({
-	awful.button({}, 3, function()
-		mymainmenu:toggle()
-	end),
-	awful.button({}, 4, awful.tag.viewprev),
-	awful.button({}, 5, awful.tag.viewnext),
-})
--- }}}
---
 -- {{{ Key bindings
 
 -- General Awesome keys
@@ -184,20 +174,6 @@ awful.keyboard.append_global_keybindings({
 		end,
 	}),
 })
-
-client.connect_signal("request::default_mousebindings", function()
-	awful.mouse.append_client_mousebindings({
-		awful.button({}, 1, function(c)
-			c:activate({ context = "mouse_click" })
-		end),
-		awful.button({ modkey }, 1, function(c)
-			c:activate({ context = "mouse_click", action = "mouse_move" })
-		end),
-		awful.button({ modkey }, 3, function(c)
-			c:activate({ context = "mouse_click", action = "mouse_resize" })
-		end),
-	})
-end)
 
 client.connect_signal("request::default_keybindings", function()
 	awful.keyboard.append_client_keybindings({
