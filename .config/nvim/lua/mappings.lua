e8c4571fb42f76a81fd696b16405ac143c3adac9
Max Braun <13639830+max-97@users.noreply.github.com>
2025-03-19 18:17:31 +0100

    Resolve clash of vim-tmux-navigator and nvim internal navigation

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,15 +4,15 @@ local map = vim.keymap.set
 
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
 map("i", "<C-e>", "<End>", { desc = "move end of line" })
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
+map("i", "<A-a>", "<Left>", { desc = "move left" })
+map("i", "<A-f>", "<Right>", { desc = "move right" })
+map("i", "<A-s>", "<Down>", { desc = "move down" })
+map("i", "<A-d>", "<Up>", { desc = "move up" })
 
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+map("n", "<A-a>", "<C-w>h", { desc = "switch window left" })
+map("n", "<A-f>", "<C-w>l", { desc = "switch window right" })
+map("n", "<A-s>", "<C-w>j", { desc = "switch window down" })
+map("n", "<A-d>", "<C-w>k", { desc = "switch window up" })
 
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
 
--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,10 +1,11 @@
 return {
 	"christoomey/vim-tmux-navigator",
+	lazy = false,
 	keys = {
-		{ "<A-a>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Move to Tmux panel left" } },
-		{ "<A-s>", "<cmd>TmuxNavigateDown<cr>", { desc = "Move to Tmux panel down" } },
-		{ "<A-d>", "<cmd>TmuxNavigateUp<cr>", { desc = "Move to Tmux panel up" } },
-		{ "<A-f>", "<cmd>TmuxNavigateRight<cr>", { desc = "Move to Tmux panel right" } },
-		{ "<A-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Move to previous Tmux panel" } },
+		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Move to Tmux panel left" } },
+		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Move to Tmux panel down" } },
+		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Move to Tmux panel up" } },
+		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Move to Tmux panel right" } },
+		{ "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Move to previous Tmux panel" } },
 	},
 }
