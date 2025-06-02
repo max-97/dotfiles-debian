f4090f74e55c146a9abf7c8bc414f106fbca1e0a
Max Braun <13639830+max-97@users.noreply.github.com>
2025-06-02 22:22:42 +0200

    Layout for desktop bar

---
+++ .config/awesome/ui/bar/init.lua
@@ -0,0 +1,80 @@
+---@diagnostic disable: undefined-global
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+local awful = require("awful")
+
+screen.conect_signal("request::desktop_decoration", function(s)
+	awful.tag({ "1", "2", "3", "4", "5", "6" }, s, awful.layout.layouts[6])
+
+	local bar_content = wibox.widget({
+		{
+			{
+				{
+					-- TODO launchers.get_launcher_widget
+					top = 7,
+					widget = wibox.container.margin,
+				},
+				nil,
+				{
+					{
+						{
+							-- systray_toggler,
+							-- dashboard_togler
+							spacing = 9,
+							layout = wibox.layout.fixed.vertical,
+						},
+						{
+							-- actions,
+							bottom = 6,
+							widget = wibox.container.margin,
+						},
+						{
+							-- clock
+							-- getlayoutbox(s),
+							-- powerbutton,
+							spacing = 2,
+							layout = wibox.layout.fixed.vertical,
+						},
+						layout = wibox.layout.fixed.vertical,
+					},
+					top = 5,
+					bottom = 5,
+					widget = wibox.container.margin,
+				},
+				layout = wibox.layout.align.vertical,
+			},
+			{
+				-- gettaglist(s),
+				halign = "center",
+				valign = "center",
+				layout = wibox.container.place,
+			},
+			layout = wibox.layout.stack,
+		},
+		bg = beautiful.bg_normal,
+		fg = beautiful.fg_normal,
+		widget = wibox.container.background,
+	})
+
+	local bar = awful.popup({
+		visible = true,
+		ontop = false,
+		minimum_height = s.geometry.height - beautiful.useless_gap * 4,
+		minimum_width = beautiful.bar_width,
+		bg = beautiful.bg_normal .. "00",
+		fg = beautiful.fg_normal,
+		widget = bar_content,
+		screen = s,
+		placement = function(d)
+			return awful.placement.left(d, {
+				margins = {
+					left = beautiful.useless_gap * 2,
+				},
+			})
+		end,
+	})
+
+	bar:struts({
+		left = beautiful.bar_width + beautiful.useless_gap * 2,
+	})
+end)
