79bbc6e82542032fd85bd84501efc893205e7691
Max Braun <13639830+max-97@users.noreply.github.com>
2025-07-08 22:26:24 +0200

    Add docker and lazydocker to setup

--- setup.sh
+++ setup.sh
@@ -32,18 +32,34 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
+# docker
+## docker's official GPG key
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 echo "***"
 echo "Install additional apps"
 echo "***"
 # install aditional apps
 sudo apt update
-sudo apt install -y brave-browser spotify-client
+sudo apt install -y brave-browser spotify-client docker-ce docker-ce-cli containerd.io docker-buildx-plugin \
+    docker-compose-plugin
 
 # Neovim latest, as of 07.07.25 it is v0.11.2
 curl -LO https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.appimage
 chmod u+x nvim-linux-x86_64.appimage
 mkdir -p /opt/nvim
 sudo mv ./nvim.appimage /opt/nvim/nvim
+
+# lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
 # NvChad
 git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
