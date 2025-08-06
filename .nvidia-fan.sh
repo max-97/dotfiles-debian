83a2a87bc941d206397d14d248f8031f90a59ca6
Max Braun <13639830+max-97@users.noreply.github.com>
2025-08-06 17:28:16 +0200

    Fix nvidia-fan script

--- .nvidia-fan.sh
+++ .nvidia-fan.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 nvidia-settings -a "[gpu:0]/GPUFanControlState=1"
-nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=40"
 
-nvidia-settings -a "[gpu:1]/GPUFanControlState=1"
+nvidia-settings -a "[fan:0]/GPUTargetFanSpeed=40"
 nvidia-settings -a "[fan:1]/GPUTargetFanSpeed=40"
