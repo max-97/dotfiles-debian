962194a2a3c4a0d12417c73a68e36a203c5af769
Max Braun <13639830+max-97@users.noreply.github.com>
2026-02-02 22:48:47 +0100

    Add dap keymappings

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -2,15 +2,24 @@ return {
 	"mfussenegger/nvim-dap",
 	keys = {
 		{ "<leader>db", "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle Breakpoint" } },
-		{
-			"<leader>dus",
-			function()
-				local widgets = require("dap.ui.widgets")
-				local sidebar = widgets.sidebar(widgets.scopes)
-				sidebar.open()
-			end,
-			{ desc = "Open debugging sidebar" },
-		},
 		{ "<leader>dr", "<cmd>DapContinue<cr>", { desc = "Start or continue debugging" } },
+		{
+			"<leader>dl",
+			function()
+				require("dap").run_last()
+			end,
+			{ desc = "Run last debugging session" },
+		},
+		{ "<leader><Down>", "<cmd>DapStepOver<cr>", { desc = "Step over" } },
+		{ "<leader><Right>", "<cmd>DapStepInto<cr>", { desc = "Step into" } },
+		{ "<leader><Left>", "<cmd>DapStepOut<cr>", { desc = "Step out" } },
+		{
+			"<leader><Up>",
+			function()
+				require("dap").restart()
+			end,
+			{ desc = "Restart session" },
+		},
+		{ "<leader>dq", "<cmd>DapTerminate<cr>", { desc = "Terminate debugging" } },
 	},
 }
