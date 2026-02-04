0bedb7ae969383ee3e935a8bbd1b7fcba2ff2a85
Max Braun <13639830+max-97@users.noreply.github.com>
2026-02-04 21:45:19 +0100

    Add keymappins for log and conditional breakpoints

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -2,6 +2,20 @@ return {
 	"mfussenegger/nvim-dap",
 	keys = {
 		{ "<leader>db", "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle Breakpoint" } },
+		{
+			"<leader>dL",
+			function()
+				require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end,
+			{ desc = "Set Breakpoint with log message" },
+		},
+		{
+			"<leader>dc",
+			function()
+				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "), nil, nil)
+			end,
+			{ desc = "Set Breakpoint with condition" },
+		},
 		{ "<leader>dr", "<cmd>DapContinue<cr>", { desc = "Start or continue debugging" } },
 		{
 			"<leader>dl",
